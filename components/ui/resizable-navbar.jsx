d556002bbb2ab10987b3da6bf9c5b05ef96a3882
mfaizk <suneater1413@gmai.com>
2025-08-03 17:04:58 +0530

    minor checks added in swap

--- components/ui/resizable-navbar.jsx
+++ components/ui/resizable-navbar.jsx
@@ -180,13 +180,8 @@ export const NavbarLogo = () => {
       href="#"
       className="relative z-20 mr-4 flex items-center space-x-2 px-2 py-1 text-sm font-normal text-black"
     >
-      <img
-        src="https://assets.aceternity.com/logo-dark.png"
-        alt="logo"
-        width={30}
-        height={30}
-      />
-      <span className="font-medium text-black dark:text-white">Startup</span>
+      <img src="/assets/logo.png" alt="logo" width={30} height={30} />
+      <span className="font-medium text-black dark:text-white">Swap</span>
     </a>
   );
 };
